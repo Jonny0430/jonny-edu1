153e768368c65abf7c6cf62cf511a720dfbaea06
Jonny0430 <ahadjon697gmail.com>
2025-05-16 23:21:21 +0900

    feat: SSR alias page & interfaces

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react';
+import { JSX, useEffect, useState } from 'react';
 import { Button, Heading, Input, Tag, Text, TextArea } from '../components';
 import Rating from '../components/rating/rating';
 import Card from '../components/card/card';
 import { withLayout } from '../layout/layout';
 import { GetServerSideProps } from 'next';
 import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
 
 
-const Index = () => {
+const Index = ({ firstCategory, menu }: HomeProps): JSX.Element  => {
 	const [isClick, setIsClick] = useState(false);
 	const [rating, setRating] = useState<number>(4);
+
+	console.log(firstCategory)
+	console.log(menu);
+
+
+	useEffect(() => {
+		const getData = async () => {
+			const { data } = await axios.post(`${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find`,{ firstCategory: 1 });
+			console.log(data);
+		};
+
+		getData();
+	}, []);
 
 	return (
 		<>
@@ -46,18 +60,34 @@ const Index = () => {
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sunt delectus ipsum accusantium. Quaerat necessitatibus
 				laborum cum quis veniam. Eveniet?
 			</Card>
+			<ul>
+				{menu.map(c => (
+					<li key={c._id.secondCategory}>{c._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	);
 };
 export default withLayout (Index);
 
 
-// export const getServerSideProps: GetServerSideProps = async () => {
-// 	const { data } = await axios.post('http://localhost:8100/page-find', { firstCategory: 1 });
 
-// 	return {
-// 		props: {
-// 			data,
-// 		},
-// 	};
-// };
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find`, { firstCategory });
+
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	};
+};
+
+
+
+
+interface HomeProps extends Record<string, unknown> {
+	firstCategory: number;
+	menu: MenuItem[];
+}
