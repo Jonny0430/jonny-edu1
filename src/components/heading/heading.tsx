bfbc1130e5d7a4ddd6508be9699878a6ad5dcdb0
Jonny0430 <ahadjon697gmail.com>
2025-05-18 13:50:24 +0900

    feat: heading.tsx {children}

--- src/components/heading/heading.tsx
+++ src/components/heading/heading.tsx
@@ -7,11 +7,11 @@ import { HeadingProps } from "./heading.props"
 const Heading = ({ tag, children}: HeadingProps): JSX.Element => {
     switch (tag) {
         case 'h1':
-            return <h1 className={styles.h1}>children</h1>
+            return <h1 className={styles.h1}>{children}</h1>
         case 'h2':
-            return <h2 className={styles.h2}>children</h2>
+            return <h2 className={styles.h2}>{children}</h2>
         case 'h3':
-            return <h3 className={styles.h3}>children</h3>
+            return <h3 className={styles.h3}>{children}</h3>
         default:
             <>{children}</>
     }
